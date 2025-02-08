00e1f4fb0f33a9347d43c8ef23ee362616b210a0
SLAINLORDS <giovanno.westin24@gmail.com>
2025-02-08 22:06:02 +0700

    SOURCE

--- no2.py
+++ no2.py
@@ -11,9 +11,9 @@ keuntungan1 = harga2-harga1
 keuntungan2 = harga4-modal2
 persen1 = (keuntungan1/modal1)*100
 persen2 = (keuntungan2/modal2)*100
-print ("Kasus Pertama:"), '/n'
+#Kasus Pertama:
 print ("keuntungan Gerald dalam kasus ini saat emas berharga 685000 adalah = ", keuntungan1)
 print ("keuntungan Gerald dalam persen saat emas berharga 685000 adalah = ",round(persen1,2),"%")
-print ("Kasus Kedua:"), '/n'
+#Kasus Kedua:
 print ("keuntungan Gerald dalam kasus ini saat emas berharga 715000 adalah = ", keuntungan2)
 print ("keuntungan Gerald dalam persen  saat emas berharga 715000 adalah = ",round(persen2,2), "%")
